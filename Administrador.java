183c8549466bfe2654c97b04e56293afdf136f9f
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2020-04-11 01:27:07 -0300

    Teste 4
    mais um java.

---
+++ Administrador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * 
+ */
+
+package br.com.hospitalif.model;
+
+public class Administrador extends Gerente {
+
+	/**
+	 * 
+	 */
+	public Administrador() {
+
+		// TODO Auto-generated constructor stub
+	}
+
+	public int cadastrarFuncionario() {
+
+		return 0;
+	}
+
+}
