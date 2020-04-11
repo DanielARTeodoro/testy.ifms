788b52797db9171ebecad90c67acea166116e9a6
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2020-04-11 01:48:54 -0300

    teste 7
    mais 1 java.

---
+++ Enfermeiro.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * 
+ */
+
+package br.com.hospitalif.model;
+
+public class Enfermeiro extends Funcionario {
+
+	private int numeroDeRegistroEnfermeiro;
+
+	/**
+	 * 
+	 */
+	public Enfermeiro() {
+
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the numeroDeRegistroEnfermeiro
+	 */
+	public int getNumeroDeRegistroEnfermeiro() {
+		return numeroDeRegistroEnfermeiro;
+	}
+
+	/**
+	 * @param numeroDeRegistroEnfermeiro the numeroDeRegistroEnfermeiro to set
+	 */
+	public void setNumeroDeRegistroEnfermeiro(int numeroDeRegistroEnfermeiro) {
+		this.numeroDeRegistroEnfermeiro = numeroDeRegistroEnfermeiro;
+	}
+
+}
