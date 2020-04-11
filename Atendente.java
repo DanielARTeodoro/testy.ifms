c17c9caf9289dc4cdde325a0756978a983f2a53b
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2020-04-11 01:34:51 -0300

    Teste 5 (#1)
    java 1.

---
+++ Atendente.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * 
+ */
+
+package br.com.hospitalif.model;
+
+public class Atendente extends Gerente {
+
+	/**
+	 * 
+	 */
+	public Atendente() {
+
+		// TODO Auto-generated constructor stub
+	}
+
+	public int cadastrarPaciente() {
+
+		return 0;
+
+	}
+}
